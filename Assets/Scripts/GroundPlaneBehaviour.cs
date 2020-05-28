76daf26a59418564c59a5a2e2ca241f527e3616c
iwasa koya <>
2020-05-28 21:37:13 +0900

    initial commit

---
+++ Assets/Scripts/GroundPlaneBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+
+public class GroundPlaneBehaviour : MonoBehaviour {
+
+    public void show() {
+        Debug.Log("Found!!");
+        this.transform.localPosition = new Vector3(0f, 0.025f, 0f);
+        this.transform.rotation = new Quaternion(0, 0, 0, 0);
+    }
+}
